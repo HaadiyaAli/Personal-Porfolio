2a336e98f9bdb1978ddc2cacc4179e949fe65538
HaadiyaAli <haadiyaali97@gmail.com>
2024-08-29 23:50:42 -0400

    Animation, Page changes finished. Next Step: Add content.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
 let body = document.getElementsByClassName('mainbody')
+let home = document.getElementsByClassName('home')
+let projects = document.getElementsByClassName('projects')
+let contacts = document.getElementsByClassName('contacts')
+
 const close = document.getElementById('close')
-const show = document.getElementById('show')
+const homeFile = document.getElementById('homeFile')
+const projectFile = document.getElementById('projectFile')
+const contactFile = document.getElementById('contactFile')
 
 close.addEventListener('click', ()=>{
     for (let i=0;i<body.length;i+=1){
@@ -9,11 +15,36 @@ close.addEventListener('click', ()=>{
        }
 })
 
-show.addEventListener('click', ()=>{
+homeFile.addEventListener('click', ()=>{
+    console.log("click")
     for (let i=0;i<body.length;i+=1){
         body[i].style.display = 'inline';
+        home[i].style.display = 'flex';
+        projects[i].style.display = 'none';
+        contacts[i].style.display = 'none';
        }
 })
+
+projectFile.addEventListener('click', ()=>{
+    console.log("click")
+    for (let i=0;i<body.length;i+=1){
+        body[i].style.display = 'inline';
+        projects[i].style.display = 'flex';
+        home[i].style.display = 'none';
+        contacts[i].style.display = 'none';
+       }
+})
+
+contactFile.addEventListener('click', ()=>{
+    console.log("click")
+    for (let i=0;i<body.length;i+=1){
+        body[i].style.display = 'inline';
+        contacts[i].style.display = 'flex';
+        projects[i].style.display = 'none';
+        home[i].style.display = 'none';
+       }
+})
+
 
 
 
